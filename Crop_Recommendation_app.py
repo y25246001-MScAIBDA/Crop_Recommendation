464ea0f4b80c6741825ef3a520ff0c9a873e4da7
y25246001-MScAIBDA <y25246001@dhsgsu.edu.in>
2026-05-01 23:38:10 +0530

    Update Crop_Recommendation_app.py

--- Crop_Recommendation_app.py
+++ Crop_Recommendation_app.py
@@ -8,12 +8,9 @@ import streamlit as st
 import numpy as np
 import pickle
 
-st.set_page_config(page_title="AgroPredict - Crop Recommendation System",
-    page_icon="🌱",
-    layout="centered",   # or "wide" if you want full screen
-    initial_sidebar_state="auto")
+st.set_page_config(page_title="Crop Recommendation System", layout="centered")
 
-st.title("🌾 AgroPredict - Crop Recommendation System")
+st.title("🌾 Crop Recommendation System")
 st.write("Enter all soil and weather values to get an accurate crop recommendation.")
 
 # Load saved models
